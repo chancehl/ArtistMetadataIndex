909e4e34d713bcff69f090213a4f5893e0f6bf13
chancehl <clinz.dev@gmail.com>
2024-02-09 21:15:13 -0800

    adds ArtistRepository class to ScanArtist task

--- src/main/java/com/chancehl/ArtistMetadataIndex/tasks/ScanArtistTask.java
+++ src/main/java/com/chancehl/ArtistMetadataIndex/tasks/ScanArtistTask.java
@@ -1,19 +1,28 @@
 package com.chancehl.ArtistMetadataIndex.tasks;
 
+import com.chancehl.ArtistMetadataIndex.interfaces.ArtistRepository;
+import com.chancehl.ArtistMetadataIndex.models.database.Artist;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.TimerTask;
 
 @Component
 @NoArgsConstructor
 public class ScanArtistTask extends TimerTask {
+    @Autowired
+    private ArtistRepository artistRepository;
+
     @Override
     public void run() {
         Logger logger = LoggerFactory.getLogger(ScanArtistTask.class);
 
-        logger.info("I am running on an interval");
+        List<Artist> artists = this.artistRepository.findAll();
+
+        logger.info("Scanning for {} artists...", artists.size());
     }
 }
--- src/main/java/com/chancehl/ArtistMetadataIndex/tasks/TaskManager.java
+++ src/main/java/com/chancehl/ArtistMetadataIndex/tasks/TaskManager.java
@@ -3,6 +3,7 @@ package com.chancehl.ArtistMetadataIndex.tasks;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Timer;
@@ -10,16 +11,18 @@ import java.util.Timer;
 @Component
 @NoArgsConstructor
 public class TaskManager {
+    @Autowired
+    private ScanArtistTask scanArtistTask;
+
     public void registerTasks() {
         Logger logger = LoggerFactory.getLogger(TaskManager.class);
 
         Timer timer = new Timer();
-        ScanArtistTask task = new ScanArtistTask();
 
-        long DELAY = 60000;
+        long DELAY = 0;
         long INTERVAL = 60000;
 
-        timer.schedule(task, DELAY, INTERVAL);
+        timer.schedule(scanArtistTask, DELAY, INTERVAL);
 
         logger.info("Successfully registered tasks");
     }
